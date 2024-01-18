4d5d58588dc9a6f4de7ec215aed94e5f35d6cb7b
vh <vihtori.hanninen@gmail.com>
2024-01-18 17:13:28 +0200

    first commit

---
+++ src/main/java/main/App.java
@@ -0,0 +1,51 @@
+package main;
+
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Hedgehog siili = new Hedgehog("Pikseli", 5);
+
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("1) Pistä siili puhumaan, 2) Luo uusi siili, 3) Juoksuta siiliä, 0) Lopeta ohjelma");
+
+            System.out.print("Valitse toiminto: ");
+            String choiceStr = sc.nextLine();
+
+            try {
+                int choice = Integer.parseInt(choiceStr);
+                switch (choice) {
+                    case 1:
+                        System.out.print("Mitä siili sanoo? ");
+                        String text = sc.nextLine();
+                        siili.speak(text);
+                        break;
+                    case 2:
+                        System.out.println("Anna siilin nimi:");
+                        String newName = sc.nextLine();
+                        System.out.println("Anna uusi ikä:");
+                        int newAge = Integer.parseInt(sc.nextLine());
+                        siili = new Hedgehog(newName, newAge);
+                        break;
+                    case 3:
+                        System.out.print("Kuinka monta kierrosta? ");
+                        int kerrat = Integer.parseInt(sc.nextLine());
+                        siili.run(kerrat);
+                        break;
+                    case 0:
+                        System.out.println("Kiitos ohjelman käytöstä.");
+                        exit = true;
+                        sc.close();
+                        break;
+                    default:
+                        System.out.println("Syöte oli väärä.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Syöte oli väärä. Yritä uudelleen.");
+            }
+        }
+    }
+}
---
+++ src/main/java/main/Hedgehog.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Hedgehog {
+    private String name;
+    private int age;
+    
+    public Hedgehog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void speak(String text) {
+        if (text.isEmpty()) {
+            System.out.println("Olen " + name + " ja ikäni on " + age + ", mutta antaisitko silti syötteen?");
+        } else {
+            System.out.println(name + ": " + text);
+        }
+    }
+
+    public void run(int kerrat) {
+        for (int i = 0; i < kerrat; i++) {
+            System.out.println(name + " juoksee kovaa vauhtia!");
+        }
+    }
+}
+
